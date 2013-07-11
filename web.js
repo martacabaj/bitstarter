0d26633c99fd408d5de9414b1d8e6039b5666390
Marta Cabaj <cabaj.marta@gmail.com>
2013-07-11 22:56:30 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var buf = new Buffer(200);
-buf.write(fs.readFile("index.html", 0);
+buf.write(fs.readFile("index.html", 0));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString("uts-8");
-
+  response.send(buf.toString("uts-8",0,12));
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
