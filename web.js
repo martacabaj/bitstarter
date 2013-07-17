30abc068fdeaa07bad049e6c1d3583a4cdd3815f
Marta Cabaj <cabaj.marta@gmail.com>
2013-07-17 21:17:26 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var fs=require("fs");
 var express = require('express');
-var text = fs.readFileSync('index.html', 'utf-8');
+var htmlfile = "index.html";
+
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
-  response.send(text);
+ var html = fs.readFileSync(htmlfile).toString(); 
+response.send(text);
 });
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
